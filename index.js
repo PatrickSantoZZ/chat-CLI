94d2b75703f1a1f5388f3e2704f7a5084c09b4c4
Nekonekoo <patrickfetscher60@gmail.com>
2020-03-06 03:06:45 +0100

    P91.04 big patch owo

--- index.js
+++ index.js
@@ -1,56 +1,96 @@
-const fs = require('fs'),
-      path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-let uwuname = null;
+
 
 module.exports = function WhisperCLI(mod) {
 	const {command} = mod.require
 
-	//message account name (your charactername)
-	mod.hook('S_LOGIN',14, (event) => {
-	uwuname = event.name
-	})
-
-	//message when target is offline
-	mod.hook('S_SYSTEM_MESSAGE',1 , (event) => {
-		if(event.message.includes('@831')) 
-		console.log('Player is not online')
-	  })
-
-
-	//message when target has blocked you
-	mod.hook('S_SYSTEM_MESSAGE',1 , (event) => {
-		if(event.message.includes('@1338')) 
-		console.log('Player has blocked you')
-	  })
+	//let area 
+	
+	let ownname = null,
+		gameId = null;
+	
+	
+	
+	//get char name
+	mod.hook('S_LOGIN',14, (event) => { ownname = event.name, gameId = event.gameId;})
+	
+	//message stuff from SYSTEM Message SMT_GENERAL_NOT_IN_THE_WORLD = offline or wrong name / SMT_BANLIST_CANT_CONTRACT_YOUR_SMH = blocked by this person 
+		mod.hook('S_SYSTEM_MESSAGE',1 , (event) => {
+			if(mod.parseSystemMessage(event.message).id === 'SMT_GENERAL_NOT_IN_THE_WORLD'){mod.log('Player is offline or wrong name')}	
+			else if(mod.parseSystemMessage(event.message).id === 'SMT_BANLIST_CANT_CONTRACT_YOUR_SMH'){mod.log('Player has blocked you')} })
 	
 	//whisper incoming
 	mod.hook('S_WHISPER', 3, (event) => {
-		fs.appendFileSync(path.join(__dirname, 'whispers.txt'), `${msgdatetime()} ${event.name} -> ${event.recipient}: ${stripOuterHTML(event.message)}\n`),
-		console.log(('[')+(uwuname)+(']')+('Whisper from')+ " " +('[')+(event.name)+(']')+ " " +('[')+('Message')+(']:')+ " " +stripOuterHTML(event.message))	
+		mod.log(('[')+(ownname)+(']')+('Whisper from')+ " " +('[')+(event.name)+(']')+ " " +('[')+('Message')+(']:')+ " " +stripOuterHTML(event.message))	
 	})
 
 	//whisp command for CLI
 	command.add('whisp', (target, ...message)=>{
-		mod.send('C_WHISPER', 1, {
-		  target: target,
-		  message: message.join(' ')
+		mod.send('C_WHISPER', 1, {target: target,message: message.join(' ')	})
+			})
+	// guild command
+	command.add('guild', (...message)=>{
+		mod.send('C_CHAT', 1, {channel: 2,message: message.join(' ')	})
+			})
+	// party command
+	command.add('party', (...message)=>{
+		mod.send('C_CHAT', 1, {channel: 1,message: message.join(' ')})
+	})
+	// trade command
+	command.add('trade', (...message)=>{
+		mod.send('C_CHAT', 1, {channel: 4,message: message.join(' ')	})
+			})
+	//global command
+	command.add('global', (...message)=>{
+		mod.send('C_CHAT', 1, {channel: 27,message: message.join(' ')	})
+			})
+	command.add('say', (...message)=>{
+		mod.send('C_CHAT', 1, {channel: 0,message: message.join(' ')	})
+			})
+	
+	mod.hook('S_CHAT',3 , (event) => {
+			if((event.channel) === 2){mod.log('[Guild] '+event.name+ " "+ stripOuterHTML(event.message) )}	
+			else if ((event.channel) === 1){mod.log('[Party] '+event.name+ " "+ stripOuterHTML(event.message)) }
+			else if ((event.channel) === 213){mod.log('[Megaphone] '+event.name+ " "+ stripOuterHTML(event.message)) }
+			else if ((event.channel) === 0){mod.log('[Say] '+event.name+ " "+ stripOuterHTML(event.message)) }
+			else if ((event.channel) === 4){mod.log('[Trade] '+event.name+ " "+ stripOuterHTML(event.message)) }
+			else if ((event.channel) === 27){mod.log('[Global] '+event.name+ " "+ stripOuterHTML(event.message)) }
+	})
+	
+
+	  var dataArray = new Buffer.alloc(1, Number());
+	  command.add('inv', (target)=>{
+		mod.send('C_REQUEST_CONTRACT', 1, { 
+			target: target,
+            type: 4,
+            name: target,
+            data: dataArray
+		})
+		mod.log(('[')+(target)+(']')+('Invited') )	
+	  })
+
+	  command.add('drop', ()=>{
+		mod.send('C_LEAVE_PARTY', 1, {
 		})
 	  })
 
-function stripOuterHTML(str) {
-	return str.replace(/^<[^>]+>|<\/[^>]+><[^\/][^>]*>|<\/[^>]+>$/g, '')
-	}
+	  command.add('add', (target, ...message) => {
+		mod.send('C_ADD_FRIEND', 1, {
+			name: target,
+			message: message.join(' ')
 
-function msgdatetime(str) {
-	var MsgDate = new Date();
-	var MsgDateString;
+		})
+		mod.log(('[')+(target)+(']')+('Added') )	
+	})
 	
-	MsgDate.setDate(MsgDate.getDate());
-	
-	MsgDateString = ('0' + MsgDate.getDate()).slice(-2) + '/' + ('0' + (MsgDate.getMonth()+1)).slice(-2) + '/' + MsgDate.getFullYear() + ' ' + ('0' + MsgDate.getHours()).slice(-2) + ':' + ('0' + MsgDate.getMinutes()).slice(-2) + ':' + ('0' + MsgDate.getSeconds()).slice(-2);
-	
-	return MsgDateString
-}
+	command.add('disband', () => {
+		mod.send('C_DISMISS_PARTY', 1, {})
+	})
+
+	function stripOuterHTML(str) {
+		return str.replace(/^<[^>]+>|<\/[^>]+><[^\/][^>]*>|<\/[^>]+>$/g, '')
+		}
 
 }
